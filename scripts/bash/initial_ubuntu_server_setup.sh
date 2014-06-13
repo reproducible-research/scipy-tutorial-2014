96f3f2d3016f3f90afe242ad8b3e0116b03beab6
Luis Ibanez <luis.ibanez@kitware.com>
2014-06-13 18:29:12 -0400

    Initial bash script to set up ubuntu server.
    Crude list of packages to install.
    
    Can serve as basis for a cooler script with Fabric or Ansible.

---
+++ scripts/bash/initial_ubuntu_server_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install apt-file
+sudo apt-file update
+sudo apt-get install apache2
+sudo apt-get install git
+sudo apt-get install build-essential
+sudo apt-get install nodejs
+sudo apt-get install npm
+sudo npm update
+sudo apt-get install tmux
+sudo apt-get install ipython
+sudo apt-get install python-dev
+sudo apt-get install python-pip
+sudo pip install numpy
