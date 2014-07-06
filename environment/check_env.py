9a915f2dab46bc90d4c6f1ddd170b2bb615946ec
Matt McCormick <matt.mccormick@kitware.com>
2014-07-05 21:46:47 -0400

    Build on Sat Jul  5 21:46:47 EDT 2014 of 2b5505f.

---
+++ environment/check_env.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+"""Computational environment validation script for the 2014 SciPy Conference Tutorial:
+
+  Reproducible Research: Walking the Walk.
+
+https://github.com/reproducible-research/scipy-tutorial-2014
+"""
+
+import sys
+import subprocess
+
+
+return_value = 0
+
+required_packages = ['numpy', 'scipy', 'matplotlib', 'SimpleITK']
+for package in required_packages:
+    print('Importing ' + package + ' ...')
+    try:
+        __import__(package, globals(), locals(), [], 0)
+    except ImportError:
+        print('Error: could not import ' + package)
+        return_value += 1
+
+
+print('')
+required_executables = ['git',
+    'dexy',
+    'ipython',
+    'latex',
+    'nosetests',
+    'mplayer']
+for executable in required_executables:
+    print('Executing ' + executable + ' ...')
+    try:
+        process = subprocess.Popen([executable, '--help'],
+                                          stderr=subprocess.STDOUT,
+                                          stdout=subprocess.PIPE)
+        process.wait()
+    except OSError:
+        print('Error: could not execute ' + executable)
+        return_value += 1
+
+
+if return_value is 0:
+    print('\nSuccess.')
+else:
+    print('\nA defect was found in your environment, please see the messages ' +
+          'above.')
+sys.exit(return_value)
