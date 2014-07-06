ad0d3b6ac8ffe761084d1bfda3c09bcb2d68495b
Matt McCormick <matt.mccormick@kitware.com>
2014-07-06 17:52:35 +0000

    Improvements to website/publish.sh.

--- website/publish.sh
+++ website/publish.sh
@@ -6,19 +6,30 @@
 #
 # Run from the top of the repository
 
-docker run -v $PWD:/home/reproducible reproducible/dexy dexy setup
-docker run -v $PWD:/home/reproducible reproducible/dexy dexy
-test $? || exit 1
+running_in_docker() {
+  awk -F/ '$2 == "docker"' /proc/self/cgroup | read
+}
+
+if test running_in_docker; then
+	dexy setup
+	dexy || exit 1
+else
+	docker run --rm -v $PWD:/home/reproducible reproducible/dexy dexy setup
+	docker run --rm -v $PWD:/home/reproducible reproducible/dexy dexy
+	test $? || exit 1
+fi
 
 repo_dir=$PWD
 cd /tmp
-git clone $repo_dir 2> /dev/null
-cd $(basename $repo_dir)
-git checkout gh-pages || git checkout -b gh-pages origin/gh-pages
+repo=$(basename $repo_dir)
+git clone $repo_dir $repo 2>/dev/null
+cd $repo
+git remote set-url origin $(cd $repo_dir; git config remote.origin.url)
 git pull
+git checkout gh-pages 2>/dev/null || git checkout -b gh-pages origin/gh-pages
 git rm -rf *
 cp -r $repo_dir/output-site/* ./
 rm ./README.md
 git add *
 git commit -m "Build on $(date) of $(cd $repo_dir; git rev-parse --short HEAD)."
-git push git@github.com:reproducible-research/scipy-tutorial-2014.git gh-pages:gh-pages
+git push origin gh-pages:gh-pages
