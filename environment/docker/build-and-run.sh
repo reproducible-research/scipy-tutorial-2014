d666c5ff499bd93ea2c21c07605b42c2e4c0ca1d
Ana Nelson <ana@ananelson.com>
2014-07-06 14:34:34 -0700

    Rename image to reproducible/base. Rename user to repro (less typing).
    Add dependency between .py files in dexy. Set loglevel to DEBUG. Tweak
    build-and-run.sh script.

--- environment/docker/build-and-run.sh
+++ environment/docker/build-and-run.sh
@@ -1,28 +1,27 @@
 #!/bin/sh
+set -e
 
 ### "build-reproducible"
-docker build -t reproducible .
+docker build -t reproducible/base .
 
 ### "build-convenience"
 docker build -t reproducible/dexy - < ./Dockerfile-dexy
 docker build -t reproducible/ipython - < ./Dockerfile-ipython
 
 ### "run-dexy"
-cd ../../../scipy-tutorial-2014/dexy
-#docker run --rm -v $PWD:/home/reproducible reproducible/dexy
+docker run --rm -v $PWD/../../dexy:/home/repro reproducible/dexy
 
 ### "start-ipython"
-cd ../../../scipy-tutorial-2014/notebooks
-docker run -d -P -v $PWD:/home/reproducible --name ipython reproducible/ipython
+docker run -d -P -v $PWD/../../notebooks:/home/reproducible --name ipython reproducible/ipython
 
-### "ipython-ps"
+#### "ipython-ps"
 docker ps
 docker port ipython 8888
 
-### "ipython-stop"
+#### "ipython-stop"
 docker stop ipython
 docker rm ipython
 
-### "docker-help"
+#### "docker-help"
 docker
 docker start
